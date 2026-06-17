ad5e9ff87d4de7ebc279cff92bb6377e370e8039
varshininarishetti-17 <varshininarishetti@gmail.com>
2026-06-17 14:01:54 +0530

    Add files via upload

---
+++ game/src/play/Game.java
@@ -0,0 +1,18 @@
+package play;
+
+import java.awt.Graphics;
+
+public interface Game {
+	public void move();
+	public void gameOver(Graphics graphics);
+
+	public void checkHit();
+
+	public void addFood();
+
+	public void checkFood();
+
+	public void playGame();
+
+	public void draw(Graphics graphics);
+}
---
+++ game/src/play/player.java
@@ -0,0 +1,7 @@
+package play;
+
+public class player {
+ public static void main(String[] args) {
+	GameFrame.Frame();	
+}
+}
